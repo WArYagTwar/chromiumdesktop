6f4c7736a83a285f26abe598fb45afd0c035445d
WArYagTwar <waryagu@pisem.net>
2017-10-08 15:54:49 +0300

    Add files via upload

--- chromiumkey.sh
+++ chromiumkey.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 wget -O ~/.local/share/applications/chromium-browser.desktop https://raw.githubusercontent.com/WArYagTwar/chromiumdesktop/master/chromium-browser.desktop
+sudo update-desktop-database
